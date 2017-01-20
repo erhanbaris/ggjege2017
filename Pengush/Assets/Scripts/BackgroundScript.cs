4168b07c76aa3ec622017a3837cd09a5db9f487d
emrnl <emreunal2@gmail.com>
2017-01-21 00:43:51 +0200

    cloud png added

---
+++ Pengush/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour {
+
+	public GameObject[] clouds;
+	public Transform[] cloudSpawnPoints;
+	public float spawnTime = 8.0f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnClouds", spawnTime, spawnTime);
+		SpawnClouds ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void SpawnClouds() {
+
+		int cloudIndex = Random.Range (0, clouds.Length);
+		int spawnIndex = Random.Range (0,cloudSpawnPoints.Length);
+
+		Instantiate (clouds[cloudIndex], cloudSpawnPoints[spawnIndex].position, cloudSpawnPoints[spawnIndex].rotation);
+
+	}
+}
---
+++ Pengush/Assets/Scripts/CloudMoveandDestroyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMoveandDestroyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (-1, 0, 0) * 3 * Time.deltaTime);
+	}
+}
