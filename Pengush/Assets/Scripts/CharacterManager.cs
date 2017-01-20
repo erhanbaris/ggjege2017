3375551bc2c971f5a5cafcc65f86ed1942417eb4
emrnl <emreunal2@gmail.com>
2017-01-20 22:11:43 +0200

    controllers

---
+++ Pengush/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+
+
+	public GameObject[] penguins;
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		penguins = new GameObject[5];
+		spawnPoints = new Transform[5];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Pengush/Assets/Scripts/PenguinControlScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinControlScript : MonoBehaviour {
+
+
+	Vector3 currentJumpVelocity;
+	static bool isJumping;
+	public int jumpingSpeed;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		Vector3 moveVelocity = Vector3.zero;
+
+		//moveVelocity.x = Input.GetAxis ("Horizontal");
+
+
+		if (Input.GetButtonDown ("Jump")) {
+			if (!isJumping) {
+				isJumping = true;
+				currentJumpVelocity = Vector3.up * 6;
+
+			}
+
+
+		}
+		if (isJumping || !controller.isGrounded) {
+
+
+			controller.Move ((moveVelocity + currentJumpVelocity) * jumpingSpeed * Time.deltaTime);
+			currentJumpVelocity += Physics.gravity * Time.deltaTime;
+			if (controller.isGrounded) {
+				isJumping = false;
+			}
+
+		} else {
+
+			//controller.SimpleMove (moveVelocity);
+			currentJumpVelocity = Vector3.zero;
+		}
+	}
+	}
+
