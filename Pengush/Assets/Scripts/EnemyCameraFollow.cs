8f78c7ce6ae11c4c2c7ed93ea4eaab1fec05a807
Erhan BARIS <erhanbaris@gmail.com>
2017-01-21 10:41:54 +0300

    Missing script file

---
+++ Pengush/Assets/Scripts/EnemyCameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCameraFollow : MonoBehaviour {
+
+	public bool StartFollow;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "ENEMY") 
+			CameraFollow.FollowEnemy = StartFollow;
+	}
+}
